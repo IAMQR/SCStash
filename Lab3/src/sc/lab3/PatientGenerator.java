7704a6eae5fc4cbbe86461fe19619bdf29f9fc0d
Mykola Demianko <mykola@demianko.com>
2018-10-17 17:21:24 +0300

    Added fields and stub methods to PatientGenerator

--- Lab3/src/sc/lab3/PatientGenerator.java
+++ Lab3/src/sc/lab3/PatientGenerator.java
@@ -1,5 +1,53 @@
 package sc.lab3;
 
+import java.util.Random;
+
 public class PatientGenerator {
- // TODO Generate classes of type Patient or at least some fields to instantiate
+	// TODO Generate classes of type Patient or at least some fields to instantiate
+	private static Random random = new Random();
+
+	private static String[] diagnosisPool;
+	private static String[] firstNamePool;
+	private static String[] lastNamePool;
+	private static String[] addressPool;
+
+	public static Patient generatePatient() {
+		// TODO
+		return null;
+	}
+
+	public static int generateIdentificationNumber() {
+		// TODO
+		return 0;
+	}
+
+	public static int generateMedicalCardNumber() {
+		// TODO
+		return 0;
+	}
+
+	public static String generateDiagnosis() {
+		// TODO
+		return null;
+	}
+
+	public static String generateFirstName() {
+		// TODO
+		return null;
+	}
+
+	public static String generateLastName() {
+		// TODO
+		return null;
+	}
+
+	public static String generateAddress() {
+		// TODO
+		return null;
+	}
+
+	public static String generatePhoneNumber() {
+		// TODO
+		return null;
+	}
 }
