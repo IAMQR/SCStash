f04edb0dd0be9033e076cfa8b8cb81fe0b029f3a
Mykola Demianko <mykola@demianko.com>
2018-10-17 20:21:38 +0300

    Arrays filled with random data

--- Lab3/src/sc/lab3/PatientGenerator.java
+++ Lab3/src/sc/lab3/PatientGenerator.java
@@ -6,10 +6,19 @@ public class PatientGenerator {
 	// TODO Generate classes of type Patient or at least some fields to instantiate
 	private static Random random = new Random();
 
-	private static String[] diagnosisPool;
-	private static String[] firstNamePool;
-	private static String[] lastNamePool;
-	private static String[] addressPool;
+	private static String[] diagnosisPool = { "Bacterial meningitis", "Blastocystis hominis infection",
+			"Reiter's syndrome", "Myoclonus", "Lupus erythematosus", "Anemia", "Black piedra",
+			"Demyelinating neuropathies", "Cold agglutinin disease", "Typhoid fever" };
+	private static String[] firstNamePool = { "Barrett", "Jayden", "Gustavo", "Cortez", "Jakob", "Nicole", "Alivia",
+			"Reagan", "Abbie", "Siena" };
+	private static String[] lastNamePool = { "Peck", "Flynn", "Ramirez", "Hatfield", "Bowers", "Sanford", "Mccormick",
+			"Forbes", "Moses", "Fisher" };
+	private static String[] addressPool = { "11 Inverness Ave. Westwood, NJ 07675",
+			"7053 Glenwood Ave. North Wales, PA 19454", "7581 Newport Court Dearborn Heights, MI 48127",
+			"8201 Homestead Street De Pere, WI 54115", "720 E. Nichols St. Palm Beach Gardens, FL 33410",
+			"807 North Gartner Street Glastonbury, CT 06033", "79 Bridge Road lymouth, MA 02360",
+			"41 Lexington St. Lady Lake, FL 32159", "9364 Goldfield Dr. Downingtown, PA 19335",
+			"746 Green Drive Rahway, NJ 07065" };
 
 	public static Patient generatePatient() {
 		// TODO
